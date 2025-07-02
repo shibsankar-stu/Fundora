76df36069882d2b3bf3203c30548d01580e12971
shibsankar-stu <dasshibsankar94732@gmail.com>
2025-07-02 10:10:44 +0530

    edit fund schemaCode

--- backend/routes/fund.routes.js
+++ backend/routes/fund.routes.js
@@ -4,6 +4,6 @@ const auth = require("../middleware/isAuth.js");
 const { searchFunds, getFundDetails } = require("../controllers/fundController.js");
 
 fundRouter.get("/search", auth, searchFunds);
-fundRouter.get("/:id", auth, getFundDetails);
+fundRouter.get("/details/:schemeCode", auth, getFundDetails);
 
 module.exports = fundRouter;
