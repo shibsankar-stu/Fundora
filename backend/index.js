09daaf062b11ae38a68a835356a52878053c39ad
shibsankar das <dasshibsankar94732@gmail.com>
2025-07-03 22:42:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser()); 
 
 app.use(cors({
-  origin: "https://fundora-frontend.onrender.com/login",
+  origin: "https://fundora-frontend.onrender.com",
   credentials: true,              
 }));
 
